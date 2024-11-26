9839ed42ea80d26709872a2ba0acde3ef55af3d7
Kiran Rana <kiranrana8973@gmail.com>
2024-11-26 12:26:19 +0545

    Added another screen

--- lib/view/first_view.dart
+++ lib/view/first_view.dart
@@ -5,7 +5,7 @@ class FirstView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(
         title: const Text('My first app'),
         centerTitle: true,
